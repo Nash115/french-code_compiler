66a51bbd176abaf5651fdf018e15509671c06c87
nash115 <72687027+Nash115@users.noreply.github.com>
2024-04-17 08:21:47 +0200

    Using the correct variable containing the source file name

--- compiler.py
+++ compiler.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     parser = parser.Parser()
     parser.produceTokens(code_file)
     # print(parser.tokens)
-    program = parser.produceAST(scanner.sys.argv[1])
+    program = parser.produceAST(code_file)
 
     print(program)
